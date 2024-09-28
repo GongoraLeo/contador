3f49fd9407ee1e257c4e54a12a48daf0462fec9f
GongoraLeo <gongora.leo@gmail.com>
2024-09-28 22:20:50 +0200

    primera versión funcional

--- index.php
+++ index.php
@@ -1,22 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contador de palabras</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <h1>Copntador de palabras</h1>
-    <p>Sencillo contador de palabras en Javascript</p>
-    <form action="">
-        <textarea name="" id="">
+    <h1>Contador de palabras</h1>
+    <h2>Sencillo contador de palabras en PHP</h2>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" id="form" name="form" method="post">
+        <textarea name="texto" id="texto">
 
         </textarea>
+        <br>
         <input type="submit" value="Contar">
         <input type="reset" value="Reset">
     </form>
+    
 
+    <?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $texto = $_POST['texto'];
+        $palabras = str_word_count($texto);
+        echo "<p>El texto es: $texto</p>";
+        echo "<p>El texto tiene $palabras palabras.</p>";
+        echo "<p>El texto tiene " . strlen($texto) . " caracteres.</p>";
+        //caracteres sin espacios
+        echo "<p>El texto tiene " . strlen(str_replace(' ', '', $texto)) . " caracteres sin espacios.</p>";
+        //número de palabras repetidas debe aparecer un minimo de 2 veces cada palabra en el texto para ser repetida decir el número de veces que se repite cada palabra
+
+        $palabras_repetidas = array_diff_assoc(str_word_count($texto, 1), array_unique(str_word_count($texto, 1)));
+        // echo "<p>Las palabras repetidas son: </p>";
+        // foreach ($palabras_repetidas as $palabra) {
+        //     echo "<p>$palabra</p>";
+        // }
+        echo "<p>El texto tiene " . count($palabras_repetidas) . " palabras repetidas.</p>";
+
+
+        //palabras no repetidas
+        // echo "<p>Las palabras no repetidas son: </p>";
+        // foreach (array_diff(str_word_count($texto, 1), $palabras_repetidas) as $palabra) {
+        //     echo "<p>$palabra</p>";
+        // }
+        //número de palabras unicas las palabras unicas son todas las palabras que aparecen al menos una vez
+        echo "<p>El texto tiene " . count(array_unique(str_word_count($texto, 1))) . " palabras unicas.</p>";
+    }
+
+    ?>
     <script src="contador.js"></script>
 </body>
+
 </html>
